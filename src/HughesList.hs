51ff8b995adf5734a5f22291e09d3a6de3956483
JasperGeer <jasper.geer@gmail.com>
2023-04-15 18:28:03 -0400

    sequence

--- src/HughesList.hs
+++ src/HughesList.hs
@@ -1,12 +1,24 @@
 module HughesList where
 
-type HughesList a = [a] -> [a]
+data List a = List ([a] -> [a])
 
-empty :: HughesList a
-empty tail = tail
+(@) :: List a -> List a -> List a
+(List f1) @ (List f2) = List (f1 . f2)
 
-singleton :: a -> HughesList a
-singleton e tail = e : tail
+(&) :: a -> List a -> List a
+a & (List f) = List (\ys -> a : (f ys))
 
-asList :: [a] -> HughesList a
-asList es tail = es ++ tail
+empty :: List a
+empty = List (\xs -> xs)
+
+singleton :: a -> List a
+singleton x = List (\xs -> x : xs)
+
+asList :: List a -> [a]
+asList (List f) = f []
+
+asHughesList :: [a] -> List a
+asHughesList xs = List (\ys -> xs ++ ys)
+
+instance Foldable List where
+  foldr f z xs = foldr f z (asList xs)
--- src/SymbolicExecution.hs
+++ src/SymbolicExecution.hs
@@ -3,12 +3,34 @@ module SymbolicExecution where
 import qualified SymbolicExpression as SX
 import qualified Env as E
 import qualified UnambiguousAST as U
+import qualified HughesList as H
+
+-- append
+(@) :: H.List a -> H.List a -> H.List a
+a @ b = a H.@ b
+
+-- cons
+(&) :: a -> H.List a -> H.List a
+a & b = a H.& b
+
+void :: a -> H.List (a, SX.Expr)
+void s = H.singleton (s, SX.I32 0)
 
 -- symbolic state: (g, rho, mu)
-
 type SymbolicState = (SX.Expr, E.Env SX.Expr, E.Env SX.Expr)
 
-type SymbolicExecutor a = SymbolicState -> a -> [SymbolicState]
+type SymbolicExecutor a = (SymbolicState, a) -> H.List (SymbolicState, SX.Expr)
+
+execmany :: SymbolicExecutor a -> H.List (SymbolicState, a) -> H.List (SymbolicState, SX.Expr)
+execmany exec = foldr (\x branches -> exec x @ branches) H.empty
+
+sequence :: SymbolicExecutor a -> SymbolicExecutor [a]
+sequence exec (s1, as) =
+  let
+    start = void s1
+    makeNewStarts branches a = foldr (\(state, _) starts -> (state, a) & starts) H.empty branches
+  in foldl (\branches a -> execmany exec (makeNewStarts branches a)) start as
 
 statement :: SymbolicExecutor U.Stmt
-statement s (U.CompoundStmt cs) = undefined
+statement (s, U.CompoundStmt cs) = undefined
+
