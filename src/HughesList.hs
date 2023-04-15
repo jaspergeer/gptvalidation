5c0423888fd5d4a4188dfad1776a07c229477380
JasperGeer <jasper.geer@gmail.com>
2023-04-15 18:51:19 -0400

    expr, list changes

--- src/HughesList.hs
+++ src/HughesList.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE InstanceSigs #-}
 module HughesList where
 
 data List a = List ([a] -> [a])
@@ -9,16 +10,21 @@ data List a = List ([a] -> [a])
 a & (List f) = List (\ys -> a : (f ys))
 
 empty :: List a
-empty = List (\xs -> xs)
+empty = List id
 
 singleton :: a -> List a
-singleton x = List (\xs -> x : xs)
+singleton x = List (x :)
 
 asList :: List a -> [a]
 asList (List f) = f []
 
 asHughesList :: [a] -> List a
-asHughesList xs = List (\ys -> xs ++ ys)
+asHughesList xs = List (xs ++)
 
 instance Foldable List where
+  foldr :: (a -> b -> b) -> b -> List a -> b
   foldr f z xs = foldr f z (asList xs)
+
+instance Functor List where
+  fmap :: (a -> b) -> List a -> List b
+  fmap f l = List (\ys -> map f (asList l) ++ ys)
--- src/SymbolicExecution.hs
+++ src/SymbolicExecution.hs
@@ -1,5 +1,7 @@
 module SymbolicExecution where
 
+import Prelude hiding (sequence)
+
 import qualified SymbolicExpression as SX
 import qualified Env as E
 import qualified UnambiguousAST as U
@@ -13,12 +15,16 @@ a @ b = a H.@ b
 (&) :: a -> H.List a -> H.List a
 a & b = a H.& b
 
+zero :: SX.Expr
+zero = SX.I32 0
+
 void :: a -> H.List (a, SX.Expr)
-void s = H.singleton (s, SX.I32 0)
+void s = H.singleton (s, zero)
 
 -- symbolic state: (g, rho, mu)
 type SymbolicState = (SX.Expr, E.Env SX.Expr, E.Env SX.Expr)
 
+-- symbolic execution with branches
 type SymbolicExecutor a = (SymbolicState, a) -> H.List (SymbolicState, SX.Expr)
 
 execmany :: SymbolicExecutor a -> H.List (SymbolicState, a) -> H.List (SymbolicState, SX.Expr)
@@ -32,5 +38,10 @@ sequence exec (s1, as) =
   in foldl (\branches a -> execmany exec (makeNewStarts branches a)) start as
 
 statement :: SymbolicExecutor U.Stmt
-statement (s, U.CompoundStmt cs) = undefined
+statement (s, c) = case c of
+  U.CompoundStmt cs -> sequence statement (s, cs)
+  U.Expr e -> fmap (\(s', _) -> (s', zero)) (expression (s, e))
+  _ -> error "TODO"
 
+expression :: SymbolicExecutor U.Expr
+expression = undefined
