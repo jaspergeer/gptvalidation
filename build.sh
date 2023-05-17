067f2c48f1869312999cabdc6bb54ca0cf91b317
JasperGeer <jasper.geer@gmail.com>
2023-05-17 11:49:08 -0700

    name change

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 echo "Building test suite..."
 cabal clean > /dev/null
-cabal run gptvalidation-testsuite 2> testsuite.out 1> /dev/null
+cabal run tinyvalidator-testsuite 2> testsuite.out 1> /dev/null
 echo "Running tests..."
 TEST_SUCCESS=`grep -q "Errors: 0  Failures: 0" testsuite.out`
 rm testsuite.out
@@ -11,4 +11,4 @@ if ! $TEST_SUCCESS; then
   exit 1
 fi
 echo "Success"
-cabal build gptvalidation
+cabal build tinyvalidator
